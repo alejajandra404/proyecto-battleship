b4893955dc6aefa37a7db1a4f844786addcdefcd
SoyYuri09 <yuri.garcia252583@potros.itson.edu.mx>
2025-09-28 11:59:42 -0700

    Creación de enumeradores
    Se crearon todos los enumeradores necesarios para el CU principal, dentro de la carpeta enums del proyecto

---
+++ code/proyecto_battleship/src/main/java/enums/EstadoPartida.java
@@ -0,0 +1,23 @@
+package enums;
+/**
+ * EstadoPartida.java
+ *
+ * Enumeración que representa los posibles estados 
+ * de una partida.
+ * 
+ * @author Leonardo Flores Leyva
+ * ID: 00000252390
+ * @author Yuri Germán García López
+ * ID: 00000252583
+ * @author Alejandra García Preciado
+ * ID: 00000252444
+ * @author Jesús Ernesto López Ibarra
+ * ID: 00000252663
+ * @author Daniel Miramontes Iribe
+ * ID: 00000252801
+ *
+ */
+public enum EstadoPartida {
+    EN_CURSO,
+    FINALIZADA
+}
---
+++ code/proyecto_battleship/src/main/java/enums/ResultadoDisparo.java
@@ -0,0 +1,23 @@
+package enums;
+/**
+ * ResultadoDisparo.java
+ *
+ * Enumeración que representa los posibles resultados 
+ * de un disparo en la partida.
+ * 
+ * @author Leonardo Flores Leyva
+ * ID: 00000252390
+ * @author Yuri Germán García López
+ * ID: 00000252583
+ * @author Alejandra García Preciado
+ * ID: 00000252444
+ * @author Jesús Ernesto López Ibarra
+ * ID: 00000252663
+ * @author Daniel Miramontes Iribe
+ * ID: 00000252801
+ *
+ */
+public enum ResultadoDisparo {
+    IMPACTO,
+    AGUA
+}
