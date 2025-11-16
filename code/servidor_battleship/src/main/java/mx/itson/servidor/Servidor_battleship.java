ef779de57747b637c22598dfeccbe0b95365199f
Ernesto López <jesus.lopez252663@potros.itson.edu.mx>
2025-11-16 16:26:46 -0700

    Update Servidor_battleship.java

--- code/servidor_battleship/src/main/java/mx/itson/servidor/Servidor_battleship.java
+++ code/servidor_battleship/src/main/java/mx/itson/servidor/Servidor_battleship.java
@@ -1,13 +1,5 @@
 package mx.itson.servidor;
 
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-import mx.itson.models.IJugador;
-import mx.itson.models.ITableroDisparos;
-import mx.itson.models.ITableroNaves;
-import mx.itson.models.JugadorServidor;
-
 /**
  *
  * @author Leonardo Flores Leyva
@@ -22,35 +14,8 @@ import mx.itson.models.JugadorServidor;
  * ID: 00000252801
  */
 public class Servidor_battleship {
-
-    private static final int PUERTO = 3306;
     
     public static void main(String[] args) {
-        
-        JugadorServidor jugador;
-        
-        System.out.println("SERVIDOR: Iniciado. Esperando jugadores...");
-        
-        try (ServerSocket serverSocket = new ServerSocket(PUERTO)) {
-            
-            // 1. Espera al primer cliente
-            Socket clienteSocket1 = serverSocket.accept();
-            System.out.println("SERVIDOR: Jugador 1 conectado: " + clienteSocket1.getInetAddress());
-            
-            // 2. Espera al segundo cliente
-            Socket clienteSocket2 = serverSocket.accept();
-            System.out.println("SERVIDOR: Jugador 2 conectado: " + clienteSocket2.getInetAddress());
-            
-            // 3. Crea las entidades del Modelo (Esto se cambiará para tomarlos desde la vista)
-            String nombre1 = "Jugador1";
-            String nombre2 = "Jugador2";
-
-            // 4. Crea la Partida
-            //IJugador jugador1 = new JugadorServidor(nombre1, tableroNaves1, tableroDisparos1);
-            //IJugador jugador2 = new JugadorServidor(nombre2, tableroNaves2, tableroDisparos2);
-
-        } catch (IOException e) {
-            System.err.println("SERVIDOR: Error al iniciar. " + e.getMessage());
-        }
+ 
     }
 }
